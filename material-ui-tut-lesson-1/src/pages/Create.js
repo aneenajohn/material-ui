e6347539a6bd4e3352170839828aa36c35ae00c3
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-27 00:00:09 +0530

    Updated container elements

--- material-ui-tut-lesson-1/src/pages/Create.js
+++ material-ui-tut-lesson-1/src/pages/Create.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+
 
 export default function Create() {
+  function submitHandler(){
+    alert('Note submitted successfully');
+  }
   return (
-    <div>
-      Create page  
-    </div>
+    <Container>
+      <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+        Create a new note
+      </Typography>
+      <Button type="submit" color="secondary" variant="contained"
+        onClick={()=> submitHandler()}
+      >
+        Submit
+      </Button>
+    </Container>
   )
 }

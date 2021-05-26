4449d1b1d24653f15d3933d3116fd4ed74116e4e
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-27 00:41:38 +0530

    added custom styles comp makeStyles

--- material-ui-tut-lesson-1/src/pages/Create.js
+++ material-ui-tut-lesson-1/src/pages/Create.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core';
+
+const useStyles= makeStyles({
+  btn:{
+    backgroundColor:'violet',
+    fontSize:"3rem",
+    '&:hover':{
+      backgroundColor:'green'
+    }
+  },
+  title:{
+    textDecoration:'underline',
+    margin:'1rem'
+  }
+})
 
 export default function Create() {
+  const cssClasess= useStyles();
   function clickHandler(){
     alert("Form submitted successfully");
   }
@@ -24,6 +41,12 @@ export default function Create() {
         <Button>Two</Button>
         <Button>Three</Button>
       </ButtonGroup>
+
+      <Typography className={cssClasess.title} variant="h5">Button with custom styles using makeStyles</Typography>
+
+      <Button
+        className={cssClasess.btn}
+      >Custom Buttom</Button>
     </div>
   )
 }

be1422314c93423ea2fc7eb44486320109b98432
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-27 00:56:30 +0530

    Added custom style

--- material-ui-tut-lesson-1/src/pages/Create.js
+++ material-ui-tut-lesson-1/src/pages/Create.js
@@ -3,15 +3,27 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import {makeStyles} from '@material-ui/core';
 
+
+const useStyles= makeStyles({
+  title:{
+    textDecoration:'underline',
+    marginTop:'1rem'
+  }
+})
 
 export default function Create() {
+
+  const classes= useStyles();
   function submitHandler(){
     alert('Note submitted successfully');
   }
   return (
     <Container>
-      <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+      <Typography variant="h6" component="h2" color="textSecondary" gutterBottom
+        className={classes.title}
+      >
         Create a new note
       </Typography>
       <Button type="submit" color="secondary" variant="contained"

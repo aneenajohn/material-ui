094c67ab3e2e34cff554fb1f3be06b9c44e89871
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-26 23:34:56 +0530

    Added Typography

--- material-ui-tut-lesson-1/src/pages/Create.js
+++ material-ui-tut-lesson-1/src/pages/Create.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Typography from "@material-ui/core/Typography";
 
 export default function Create() {
   return (
     <div>
-      Create page  
+      <Typography variant="h2" align="center" color="primary">  
+        Create a new note
+      </Typography>
+      <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+        Note 1
+      </Typography>
+      <Typography color="secondary">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </Typography>
+      <Typography noWrap color="textPrimary">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </Typography>
     </div>
+ 
   )
 }

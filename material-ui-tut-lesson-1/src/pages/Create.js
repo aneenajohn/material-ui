f90efe1be5e8221e073d3d988d91f2c903f15397
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-27 00:26:20 +0530

    Added Icons

--- material-ui-tut-lesson-1/src/pages/Create.js
+++ material-ui-tut-lesson-1/src/pages/Create.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Typography from "@material-ui/core/Typography";
+import AppleIcon from '@material-ui/icons/Apple';
+import Button from '@material-ui/core/Button';
+import SendIcon from '@material-ui/icons/Send';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 export default function Create() {
   return (
@@ -16,6 +20,18 @@ export default function Create() {
       <Typography noWrap color="textPrimary">
       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
       </Typography>
+      <br/>
+      <AppleIcon/>
+      <AppleIcon color="secondary" fontSize="large" />
+      <AppleIcon color="action" fontSize="small" />
+      <AppleIcon color="error" fontSize="large" />
+      <AppleIcon color="disabled" />
+      <br/>
+      <Button variant="contained" color="primary"
+       startIcon={<SendIcon/>}
+       endIcon={<KeyboardArrowRightIcon/>}
+      >Send</Button>
+
     </div>
  
   )

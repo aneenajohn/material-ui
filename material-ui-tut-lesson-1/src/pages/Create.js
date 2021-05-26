7654a32bf8d60dadbe43ea6ce7f484d3478be9c6
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-26 23:52:22 +0530

    Added Button , ButtonGroup

--- material-ui-tut-lesson-1/src/pages/Create.js
+++ material-ui-tut-lesson-1/src/pages/Create.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 export default function Create() {
+  function clickHandler(){
+  }
   return (
     <div>
-      Create page  
+      <Button type="submit" color="secondary" 
+        onClick={()=> clickHandler()}
+      >
+        Submit
+      </Button>
+      <Button color="primary" variant="contained">
+        Home
+      </Button>
+      <Button color="primary" variant="outlined" disableElevation>
+        About
+      </Button>
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
     </div>
   )
 }

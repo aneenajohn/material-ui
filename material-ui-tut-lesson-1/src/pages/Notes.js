4a81f2dffcf3c42b811478a36679110802fee3dc
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-27 19:17:16 +0530

    Added grid

--- material-ui-tut-lesson-1/src/pages/Notes.js
+++ material-ui-tut-lesson-1/src/pages/Notes.js
@@ -2,6 +2,8 @@ import React from 'react'
 import axios from 'axios';
 import {useEffect, useState} from 'react';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 export default function Notes() {
   const [notes,setNotes]= useState([]);
@@ -15,6 +17,14 @@ export default function Notes() {
   return (
     <div>
       <Typography>Notes</Typography>
+      <Grid container>
+        <Grid item xs={12} sm={6} md={3}>1</Grid>
+        <Grid item xs={12} sm={6} md={3}>2</Grid>
+        <Grid item xs={12} sm={6} md={3}>3</Grid>
+        <Grid item xs={12} sm={6} md={3}>4</Grid>
+        <Grid item xs={12} sm={6} md={3}>5</Grid>
+        <Grid item xs={12} sm={6} md={3}>6</Grid>
+      </Grid>
       {notes.map(note => (
         <Typography>{note.title}</Typography>
       ))}

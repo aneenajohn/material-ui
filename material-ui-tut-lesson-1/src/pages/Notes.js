9688d2555354d7a5a6ca8f4cec2b40f94379ae9f
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-27 19:49:14 +0530

    Updated notes grid

--- material-ui-tut-lesson-1/src/pages/Notes.js
+++ material-ui-tut-lesson-1/src/pages/Notes.js
@@ -4,9 +4,17 @@ import {useEffect, useState} from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
+import { makeStyles} from '@material-ui/core';
 
+const useStyles = makeStyles({
+  title:{
+    margin:'1rem 0',
+  }
+})
 export default function Notes() {
   const [notes,setNotes]= useState([]);
+  const classes = useStyles();
   useEffect(() => {
     ( async function(){
       const {data} = await axios.get("http://localhost:8000/notes");
@@ -15,19 +23,23 @@ export default function Notes() {
     })()
   }, [])
   return (
-    <div>
-      <Typography>Notes</Typography>
-      <Grid container>
+    <Container>
+      <Typography className={classes.title}>Notes</Typography>
+      {/* <Grid container>
         <Grid item xs={12} sm={6} md={3}>1</Grid>
         <Grid item xs={12} sm={6} md={3}>2</Grid>
         <Grid item xs={12} sm={6} md={3}>3</Grid>
         <Grid item xs={12} sm={6} md={3}>4</Grid>
         <Grid item xs={12} sm={6} md={3}>5</Grid>
         <Grid item xs={12} sm={6} md={3}>6</Grid>
+      </Grid> */}
+      <Grid container >
+        {notes.map(note => (
+          <Grid item key={note.id} xs={12} md={6} lg={4}> 
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
       </Grid>
-      {notes.map(note => (
-        <Typography>{note.title}</Typography>
-      ))}
-    </div>
+    </Container>
   )
 }

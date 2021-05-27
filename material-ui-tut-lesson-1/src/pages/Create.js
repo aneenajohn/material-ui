3e9ddf4f6c238c1e2a8244ecbfdea3f68464c3a2
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-27 19:15:42 +0530

    Added Grid

--- material-ui-tut-lesson-1/src/pages/Create.js
+++ material-ui-tut-lesson-1/src/pages/Create.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 export default function Create() {
   function clickHandler(){
@@ -24,6 +26,14 @@ export default function Create() {
         <Button>Two</Button>
         <Button>Three</Button>
       </ButtonGroup>
+      <Grid container>
+        <Grid item xs={12} sm={6} md={3}>1</Grid>
+        <Grid item xs={12} sm={6} md={3}>2</Grid>
+        <Grid item xs={12} sm={6} md={3}>3</Grid>
+        <Grid item xs={12} sm={6} md={3}>4</Grid>
+        <Grid item xs={12} sm={6} md={3}>5</Grid>
+        <Grid item xs={12} sm={6} md={3}>6</Grid>
+      </Grid>
     </div>
   )
 }

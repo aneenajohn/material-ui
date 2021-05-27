c95cb695622fd62077fd82c35cdb6a6269980d4a
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-27 10:42:47 +0530

    Added textField with validation

--- material-ui-tut-lesson-1/src/pages/Create.js
+++ material-ui-tut-lesson-1/src/pages/Create.js
@@ -1,24 +1,46 @@
 import React from 'react';
+import {useState} from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
-import {makeStyles} from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import {AccordionDetails, makeStyles} from '@material-ui/core';
 
 
 const useStyles= makeStyles({
   title:{
     textDecoration:'underline',
     marginTop:'1rem'
+  },
+  field:{
+    margin : "2rem 0rem",
+    display: "block"
   }
 })
 
 export default function Create() {
 
   const classes= useStyles();
-  function submitHandler(){
-    alert('Note submitted successfully');
+  // function submitHandler(){
+  //   alert('Note submitted successfully');
+  // }
+
+  const handleSubmit = (e) =>{
+    setTitleError(false);
+    setDetailsError(false);
+    title === "" && setTitleError(true);
+    details === "" && setDetailsError(true);
+      e.preventDefault();
+      ( (title && details) && (
+        console.log("title:", title,"details:",details)
+      ))
   }
+
+  const [title,setTitle] = useState("");
+  const [details,setDetails] = useState("");
+  const [titleError,setTitleError] = useState(false);
+  const [detailsError,setDetailsError] = useState(false);
   return (
     <Container>
       <Typography variant="h6" component="h2" color="textSecondary" gutterBottom
@@ -26,12 +48,35 @@ export default function Create() {
       >
         Create a new note
       </Typography>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField className={classes.field}
+          onChange={(e)=>setTitle(e.target.value)}
+          label="Note Title"
+          variant="outlined"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField className={classes.field}
+          onChange={(e)=> setDetails(e.target.value)}
+          label="Details"
+          variant="outlined"
+          fullWidth
+          required
+          multiline
+          rows={4}
+          error={detailsError}
+        />
+
       <Button type="submit" color="secondary" variant="contained"
-        onClick={()=> submitHandler()}
+        // onClick={()=> submitHandler()}
         endIcon={<KeyboardArrowRightIcon/>}
       >
         Submit
       </Button>
+
+      </form>
+      
     </Container>
   )
 }

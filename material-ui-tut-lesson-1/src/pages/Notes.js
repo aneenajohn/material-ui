97e3810aeadbac86319a41690425e8d665b929cf
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-28 19:25:51 +0530

    Added masonary css

--- material-ui-tut-lesson-1/src/pages/Notes.js
+++ material-ui-tut-lesson-1/src/pages/Notes.js
@@ -7,6 +7,7 @@ import Paper from '@material-ui/core/Paper';
 import Container from '@material-ui/core/Container';
 import { makeStyles} from '@material-ui/core';
 import NoteCard from '../components/NoteCard';
+import Masonry from 'react-masonry-css';
 
 const useStyles = makeStyles({
   title:{
@@ -29,24 +30,23 @@ export default function Notes() {
     const newNotes = notes.filter(note => note.id !== id);
     setNotes(newNotes);
   }
+  const breakpointColumnsObj={
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  }
   return (
     <Container>
-      {/* <Typography className={classes.title} variant="h5">Notes</Typography> */}
-      {/* <Grid container>
-        <Grid item xs={12} sm={6} md={3}>1</Grid>
-        <Grid item xs={12} sm={6} md={3}>2</Grid>
-        <Grid item xs={12} sm={6} md={3}>3</Grid>
-        <Grid item xs={12} sm={6} md={3}>4</Grid>
-        <Grid item xs={12} sm={6} md={3}>5</Grid>
-        <Grid item xs={12} sm={6} md={3}>6</Grid>
-      </Grid> */}
-      <Grid container spacing={3}>
+      <Masonry  breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column">
         {notes.map(note => (
-          <Grid item key={note.id} xs={12} sm={6} md={6} lg={4}> 
+          <div key={note.id} xs={12} sm={6} md={6} lg={4}> 
             <NoteCard note={note} handleDelete={handleDelete} />
-          </Grid>
+          </div>
         ))}
-      </Grid>
+      </Masonry>
     </Container>
   )
 }
